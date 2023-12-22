558528c1600c8314d10fd8503570d522a3710ec4
Cleiton Viana <cleitonviana.2@outlook.com.br>
2023-12-22 11:53:42 -0300

    página inicial concluída...

--- scripts/fetch.js
+++
@@ -1,5 +0,0 @@
-export default async (url) => {
-    const baseUrl = `https://rickandmortyapi.com/api/`
-        const res = await fetch(`${baseUrl + url}`)
-        return await res.json()
-}
--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,47 @@
-const cardsContainer = document.querySelector('.cards__container')
-
 const url = `https://rickandmortyapi.com/api/character/`
 let nextUrl = null
+const charactersList = []
 
-const btnLoadMoreChars = document.querySelector('button').addEventListener('click', loadMoreChars)
+const cardsContainer = document.querySelector('.cards__container')
+
+const btnLoadMoreChars = document.querySelector('button')
+btnLoadMoreChars.addEventListener('click', loadMoreChars)
+
+const inputSearch = document.querySelector('#inpSearch')
+inputSearch.addEventListener('input', searchPerson)
 
 const specie = document.querySelector('#species')
-const gender = document.querySelector('#gender')
-const statusFilter = document.querySelector('#status')
-
 specie.addEventListener('input', filterCharacters)
+
+const gender = document.querySelector('#gender')
 gender.addEventListener('input', filterCharacters)
+
+const statusFilter = document.querySelector('#status')
 statusFilter.addEventListener('input', filterCharacters)
 
 
-function fetchDatas(url) {
-    console.log(url)
+
+async function fetchDatas(url) {
     fetch(url)
         .then(res => res.json())
         .then(res => {
-            console.log(res)
+
             nextUrl = res.info.next
+
+            if(nextUrl === null) {
+                btnLoadMoreChars.classList.add('display-none')
+            }
+
             createCard(res.results)
         })
 }
 
+function loadChars(param){
+    const chars = (param) ? param : charactersList
+    chars.forEach(el => {
+        cardsContainer.appendChild(el)
+    })
+}
 
 function loadMoreChars() {
     fetchDatas(nextUrl)
@@ -46,11 +63,8 @@ function filterCharacters() {
 
 
     if (filters.length > 0) {
-
         filters = filters.join('&')
-
         newUrl = url + '?' + filters
-        console.log(newUrl)
     } else {
         newUrl = url
     }
@@ -59,11 +73,9 @@ function filterCharacters() {
 }
 
 function createCard(characters) {
-
     characters.forEach((char) => {
         const card = document.createElement('div')
-        card.classList.add('card--character')
-        card.classList.add('card')
+        card.classList.add('card--character', 'card')
 
         const a = document.createElement('a')
         a.setAttribute('href', `/character/${char.id}`)
@@ -74,27 +86,54 @@ function createCard(characters) {
 
         const div = document.createElement('div')
 
-        const h3 = document.createElement('h3')
-        h3.textContent = char.name
+        const name = document.createElement('h3')
+        name.textContent = char.name
 
-        const p = document.createElement('p')
-        p.textContent = char.species
+        const specie = document.createElement('p')
+        specie.textContent = char.species
 
+        const status = document.createElement('p')
+        status.textContent = char.status
+        const span = document.createElement('span')
+        span.classList.add(char.status.toLowerCase(), 'circle')
+        span.style.marginLeft = '8px'
 
         card.appendChild(a)
         a.appendChild(img)
         card.appendChild(div)
-        div.appendChild(h3)
-        div.appendChild(p)
+        div.appendChild(name)
+        div.appendChild(specie)
+        div.appendChild(status)
+        status.appendChild(span)
 
-
+        charactersList.push(card)
         cardsContainer.appendChild(card)
     })
 }
 
+function searchPerson() {
+    if(inputSearch.value.trim() === '') return loadChars()
+    clearCards()
+
+    specie.value = 'all'
+    gender.value = 'all'
+    statusFilter.value = 'all'
+
+    const characterSearch = []
+
+    charactersList.forEach(char => {
+        const name = char.querySelector('h3').textContent
+
+        if(name.toLowerCase().includes(inputSearch.value.toLowerCase())) {
+            characterSearch.push(char)
+        }
+    })
+
+    loadChars(characterSearch)
+}
+
+
 fetchDatas(url)
-
-
 
 
 
